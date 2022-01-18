1ce57ad527c535dd3da6a88b722b6cf27023c04f
sixcious <1027814+sixcious@users.noreply.github.com>
2022-01-18 14:31:03 -0500

    Update cryptography.js

--- cryptography.js
+++ cryptography.js
@@ -25,7 +25,7 @@
  * const hash = await Cryptography.hash("plaintext", salt);
  *
  * 2. Encrypting/Decrypting:
- * Generate a secret key however you prefer (for quick demonstration purposes,this uses Cryptography.salt())
+ * Generate a secret key however you prefer (for quick demonstration purposes, this uses Cryptography.salt())
  * const secret = Cryptography.salt();
  * const encryption = await Cryptography.encrypt("plaintext", secret);
  * const decryption = await Cryptography.decrypt(encryption.ciphertext, encryption.iv, secret);
